f1031c41540b7c8fb91596f1636214ddb9638b46
Şuayip Işık <suayip.isik.16@gmail.com>
2023-02-03 01:10:52 +0300

    product listed

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,12 +1,31 @@
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
-import React from 'react'
 import styles from "./Products.style";
 import Config from 'react-native-config';
+import { FlatList } from 'react-native';
+import axios from 'axios';
+import ProductCard from '../../components/ProductCard';
 
 const Products = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        try {
+            const { data: responseData } = await axios.get(Config.API_URL)
+            setData(responseData)
+        } catch (e) {
+
+        }
+    }
+
+    const renderProduct = ({ item }) => <ProductCard product={item} />
     return (
         <View>
-            <Text>Products {Config.API_URL}</Text>
+            <FlatList data={data} renderItem={renderProduct} />
         </View>
     )
 }
