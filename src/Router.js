5022a788c544c724bd104bbf466796730eeedbf9
Şuayip Işık <suayip.isik.16@gmail.com>
2023-02-02 19:15:07 +0300

    App.js changed to Router.js and Router.js changed file directory

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import { Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Router = () => {
+  return (
+    <SafeAreaView>
+      <Text>Router</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Router
