a35ddc3b2b51bf7d5ca2088ae62f0574a5190e26
Şuayip Işık <suayip.isik.16@gmail.com>
2023-02-03 00:00:29 +0300

    react-native-config (.env) added and executed

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import styles from "./Products.style";
+import Config from 'react-native-config';
 
 const Products = () => {
     return (
         <View>
-            <Text>Products</Text>
+            <Text>Products {Config.API_URL}</Text>
         </View>
     )
 }
